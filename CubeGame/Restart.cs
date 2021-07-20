5bda741404626ac10f3e5aba6b342bd2a2b34ff9
miiifly <63255043+miiifly@users.noreply.github.com>
2021-07-20 11:24:29 +0300

    Add files via upload

---
+++ CubeGame/Restart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour
+{
+    private Scene scene;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(scene.name);
+
+        }
+        else
+        {
+            
+        }
+    }
+    }
