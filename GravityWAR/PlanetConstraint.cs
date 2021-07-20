4eeb3bd29303d86155db6ee3c9cc267edef4530d
miiifly <63255043+miiifly@users.noreply.github.com>
2021-07-20 11:30:30 +0300

    Add files via upload

---
+++ GravityWAR/PlanetConstraint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlanetConstraint : MonoBehaviour
+{
+    public Transform TargetPlanet;
+    
+    private void FixedUpdate()
+    {
+        Quaternion rotation = Quaternion.FromToRotation(-transform.up, TargetPlanet.position - transform.position);
+        transform.rotation = rotation * transform.rotation;
+    }
+}
